3de8735d6428d86725d8a4ab96e1fe7d10f16d52
Benjamin Simon <wizkid686@gmail.com>
2016-04-06 14:18:22 -0400

    Use email indexes to prevent duplication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :first_name, presence: true, length: { maximum: 25 }
   validates :last_name, presence: true, length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
