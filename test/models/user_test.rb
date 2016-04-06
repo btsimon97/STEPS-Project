9b6c78b4358cc293ce58facb26ceb449b14f43f2
Benjamin Simon <wizkid686@gmail.com>
2016-04-06 17:49:59 -0400

    Fix application permissions

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(first_name: "John", last_name: "Dogn", email:"jdogn@example.com")
+    @user = User.new(first_name: "John", last_name: "Dogn", email:"jdogn@example.com", 
+                     password: "foobar12", password_confirmation: "foobar12")
   end
 
   test "should be valid" do
@@ -63,5 +64,15 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 8
+    assert_not @user.valid?
+  end
+
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 7
+    assert_not @user.valid?
   end
 end
