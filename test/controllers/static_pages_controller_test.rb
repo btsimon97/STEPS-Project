372e3905fc26758650342890685d6720d600bac8
Benjamin Simon <wizkid686@gmail.com>
2016-04-05 21:40:19 -0400

    Improve Static Page Tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "The STEPS Project"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","Home | The STEPS Project" 
+    assert_select "title","Home | #{@base_title}" 
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title","Help | The STEPS Project"
+    assert_select "title","Help | #{@base_title}"
   end
 
   test "shoud get about" do
     get :about
     assert_response :success
-    assert_select "title","About | The STEPS Project"
+    assert_select "title","About | #{@base_title}"
   end
 end
