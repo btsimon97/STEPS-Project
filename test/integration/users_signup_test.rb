dd8586a43f93afb775c259050a78216e7746e184
Benjamin Simon <wizkid686@gmail.com>
2016-04-07 09:51:49 -0400

    Add tests for flash messages

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_not flash.nil?
     assert_select 'div#{CSS id for error explanation>'
     assert_select 'div.<CSS class for field with error>'
   end
