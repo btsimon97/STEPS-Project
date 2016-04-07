f9795e8cbb9d64d77874bf4670cc2fa975125807
Benjamin Simon <wizkid686@gmail.com>
2016-04-07 12:22:10 -0400

    Do autologin on signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { first_name:  "Example",
+                                            last_name: "User",
+                                            user_id_number: "123456",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
 end
