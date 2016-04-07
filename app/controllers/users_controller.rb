5e084cd72c0ffb57f84b95876d552b35159e2817
Benjamin Simon <wizkid686@gmail.com>
2016-04-07 11:45:10 -0400

    Add user login page and supporting tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to The STEPS Project!"
+      flash.now[:success] = "Welcome to The STEPS Project!"
       redirect_to @user
     else
       render 'new'
