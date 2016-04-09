2a353e3b0bde918336940791e3ce3b965b846d24
Benjamin Simon <wizkid686@gmail.com>
2016-04-08 20:56:56 -0400

    Add facilities for generating fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+#This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(first_name:            "Example",
+             last_name:             "User",
+             user_id_number:        123456,
+             email:                 "example@stepsproject.net",
+             password:              "foobar12",
+             password_confirmation: "foobar12")
+
+99.times do |n|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  user_id_number = Faker::Number.number(6)
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(first_name: first_name,
+               last_name: last_name,
+               user_id_number: user_id_number,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
