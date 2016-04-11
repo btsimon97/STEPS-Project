d1415e3bf2724390b8ceeec6ae2cbae4e23396b2
Ben Simon <wizkid686@gmail.com>
2016-04-11 13:05:13 -0400

    Remove custom auth scheme and switch to devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
-  get 'static_pages/home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
