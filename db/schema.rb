fdca36192c696259a4eaeefa02d9fccbe6afb192
Ben Simon <wizkid686@gmail.com>
2016-04-18 13:59:12 -0400

    Run DB migrations to add students, courses, and teacher connections

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413025043) do
+ActiveRecord::Schema.define(version: 20160418174813) do
+
+  create_table "course_students", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "course_id"
+  end
+
+  add_index "course_students", ["course_id"], name: "index_course_students_on_course_id"
+  add_index "course_students", ["user_id"], name: "index_course_students_on_user_id"
+
+  create_table "course_teachers", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "course_id"
+  end
+
+  add_index "course_teachers", ["course_id"], name: "index_course_teachers_on_course_id"
+  add_index "course_teachers", ["user_id"], name: "index_course_teachers_on_user_id"
+
+  create_table "courses", force: :cascade do |t|
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
