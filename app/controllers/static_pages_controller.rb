76f8e38fe767f0566f5e2eeffae660f745c73a6a
Ben Simon <wizkid686@gmail.com>
2016-04-27 23:00:49 -0400

    Fix error on user signout related to dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @courses = current_user.courses.paginate(page: params[:page], per_page:5)
+    if user_signed_in?
+      @courses = current_user.courses.paginate(page: params[:page], per_page:5)
+    end
   end
 
   def help
