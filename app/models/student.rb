19d6473c32c5920bc0409f0153cb6f6c784fd580
Ben Simon <wizkid686@gmail.com>
2016-05-03 14:30:46 -0400

    Add Students Through Devise

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ActiveRecord::Base
-  has_many :courses
-  has_many :teachers, through: :courses
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428214038) do
+ActiveRecord::Schema.define(version: 20160503182313) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "course_name"
     t.integer  "course_sequence_number"
     t.integer  "user_id"
+    t.integer  "student_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "period"
   end
 
   add_index "courses", ["period"], name: "index_courses_on_period"
+  add_index "courses", ["student_id"], name: "index_courses_on_student_id"
   add_index "courses", ["user_id", "course_name"], name: "index_courses_on_user_id_and_course_name"
   add_index "courses", ["user_id"], name: "index_courses_on_user_id"
 
@@ -30,9 +32,22 @@ ActiveRecord::Schema.define(version: 20160428214038) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "student_number"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
+
+  add_index "students", ["email"], name: "index_students_on_email", unique: true
+  add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
