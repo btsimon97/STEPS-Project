d1202da6dcb0322053d035e2e1d120fe65e3fff2
Ben Simon <wizkid686@gmail.com>
2016-05-03 14:53:55 -0400

    Add Student Parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,13 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) << :first_name
       devise_parameter_sanitizer.for(:sign_up) << :last_name
       devise_parameter_sanitizer.for(:sign_up) << :user_number 
+      devise_parameter_sanitizer.for(:sign_up) << :student_number
       devise_parameter_sanitizer.for(:sign_up) << :avatar
       devise_parameter_sanitizer.for(:sign_up) << :subject_area
       devise_parameter_sanitizer.for(:account_update) << :first_name
       devise_parameter_sanitizer.for(:account_update) << :last_name
       devise_parameter_sanitizer.for(:account_update) << :user_number
+      devise_parameter_sanitizer.for(:account_update) << :student_number
       devise_parameter_sanitizer.for(:account_update) << :avatar
       devise_parameter_sanitizer.for(:account_update) << :subject_area
       devise_parameter_sanitizer.for(:account_update) << :instructional_level
