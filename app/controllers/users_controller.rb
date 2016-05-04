33ad4bd20a76e4764ab44fcf26be44f2dac77ad5
Ben Simon <wizkid686@users.noreply.github.com>
2016-05-04 14:11:52 -0400

    Fix sorting when viewing a specific user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,21 +8,21 @@ class UsersController < ApplicationController
   end
 
   def index
-    @user = User.all
     @users = User.order(sort_column + " " + sort_direction).paginate(page: params[:page])
   end
 
   private
 
   def sort_column
-    Course.column_names.include?(params[:sort]) ? params[:sort] : 'course_name'
-    Course.column_names.include?(params[:sort]) ? params[:sort] : 'course_sequence_number'
-    Course.column_names.include?(params[:sort]) ? params[:sort] : 'period'
-    User.column_names.include?(params[:sort]) ? params[:sort] : 'first_name'
-    User.column_names.include?(params[:sort]) ? params[:sort] : 'last_name'
-    User.column_names.include?(params[:sort]) ? params[:sort] : 'subject_area'
-    User.column_names.include?(params[:sort]) ? params[:sort] : 'instructional_level'
-    User.column_names.include?(params[:sort]) ? params[:sort] : 'school_name'
+   # Course.
+    if action_name.to_s == 'show'
+      Course.column_names.include?(params[:sort]) ? params[:sort] : 'course_name'
+    elsif action_name.to_s == 'index'
+      User.column_names.include?(params[:sort]) ? params[:sort] : 'school_name'
+    else
+      Course.column_names.include?(params[:sort]) ? params[:sort] : 'course_name'
+      User.column_names.include?(params[:sort]) ? params[:sort] : 'school_name'
+    end
   end
 
   def sort_direction
